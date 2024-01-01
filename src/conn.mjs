ddbd06564e6457064568999a83cc6c4301e0b730
hhejo <litsmainjof@gmail.com>
2024-01-01 21:01:47 +0900

    feat: ObjectId 추가

--- src/conn.mjs
+++ src/conn.mjs
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb";
 import { MONGODB_URI, MONGODB_DB_NAME } from "./load-environment.mjs";
+export { ObjectId } from "mongodb";
 
 const client = new MongoClient(MONGODB_URI);
 const db = client.db(MONGODB_DB_NAME);
